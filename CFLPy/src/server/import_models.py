87dfcf9fc9fb8f81558f364192e55032eae47721
Pascal MARIE <saphyr.pascal@sfr.fr>
2017-10-28 17:10:47 +0200

    mv CFLPy

--- CFLPy/src/server/import_models.py
+++
@@ -1,7 +0,0 @@
-import devpy.develop as log
-
-log.info("Initializing MongoDB Documents...")
-
-from src.server.models.defunt import Defunt
-
-log.info("...done")
--- CFLPy/src/settings.py
+++
@@ -1,7 +0,0 @@
-class Config:
-    app_name = "CFL"
-    db_name = "CFL"
-    db_host = "localhost"
-    db_port = 27017
-
-    DEBUG = True
