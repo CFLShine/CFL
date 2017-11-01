8a5f71700697dcda8d048e2e9e0dfc51c3555c70
Tristan MARIE <saphyr73@hotmail.fr>
2017-11-01 18:30:46 +0100

    Correction de bug

--- src/server/__init__.py
+++ src/server/__init__.py
@@ -5,12 +5,13 @@ from src.client.start import start
 from src.server.import_models import *
 from src.settings import Config
 
-# Connexion à la Database
-log.info(f"Connecting to database [{Config.db_name}@{Config.db_host}:{Config.db_port}]")
-connect(Config.db_name, host=Config.db_host, port=Config.db_port)
+if __name__ == '__main__':
+    # Connexion à la Database
+    log.info(f"Connecting to database [{Config.db_name}@{Config.db_host}:{Config.db_port}]")
+    connect(Config.db_name, host=Config.db_host, port=Config.db_port)
 
 
-# Création et démarrage de l'appli Flask
-log.info("Starting Flask app")
-app = Flask(__name__, template_folder='../client/templates')
-start(app)
+    # Création et démarrage de l'appli Flask
+    log.info("Starting Flask app")
+    app = Flask(__name__, template_folder='../client/templates')
+    start(app)
