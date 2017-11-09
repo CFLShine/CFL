bea459711ddfd742edaebbe3ecb7c94b55ff5741
Tristan MARIE <saphyr73@hotmail.fr>
2017-11-09 13:39:48 +0100

    Modification fiche défunt pour qu'elle affiche un défunt
    Signed-off-by: Tristan MARIE <saphyr73@hotmail.fr>

--- src/client/blueprints/fiche_defunt/fiche_defunt.py
+++ src/client/blueprints/fiche_defunt/fiche_defunt.py
@@ -1,11 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
+from settings import Config
 
 fiche_defunt = Blueprint('fiche_defunt', __name__,
                          template_folder='templates')
 
 
-@fiche_defunt.route('/fiche_defunt/')
-def show():
-    pass
+@fiche_defunt.route('/fiche_defunt/<dftid>')
+def show(dftid):
+    from src.server.cfl_data.defunt.defunt import Defunt
+    from src.server.populate_db.pdb_populate import populate
 
-    # return render_template('fiche_defunt.html', config=Config, page_name='fiche_defunt', dfts=Defunt.objects)
+    dft = Defunt.objects(id=dftid)[0]
+    return render_template('fiche_defunt.html', config=Config, page_name='fiche_defunt', dft=dft)
